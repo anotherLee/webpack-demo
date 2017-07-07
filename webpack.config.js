e993667c628086b36359638270ed946e973d5e14
limingru <18813070947@163.com>
2017-07-07 13:54:28 +0800

    webpack -p

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -6,11 +9,34 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Webpack-demos',
+      filename: 'index.html'
+    }),
+    new OpenBrowserPlugin({
+      url: 'http://localhost:9000'
+    })
+  ],
+
   module:{
     loaders:[
       {
         test: /\.css$/,
         loader: 'style-loader!css-loader!autoprefixer-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+           presets: ["es2015"]
+         }
       },
       {
         test: /\.(png|jpg)$/,
