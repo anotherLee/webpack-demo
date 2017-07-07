77f290ac1ca549311866257fc4fd95c2a585e43a
limingru <18813070947@163.com>
2017-07-07 11:40:09 +0800

    使用less sass css image并压缩image

---
+++ src/limingru.js
@@ -0,0 +1,10 @@
+let name = 'limingru'
+let age = 18
+let gender = "male"
+
+function sayHi(){
+  console.log("Hi, limingru!")
+}
+
+
+export {name, age, gender, sayHi}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module:{
+    loaders:[
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!autoprefixer-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  }
+};
